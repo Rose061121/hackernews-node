db105705c81670dd532c64913ad7eca9edba9894
Rose Sawyer <rosesawyer@Roses-MacBook-Air.local>
2020-01-31 11:34:22 -0500

    added database

--- src/index.js
+++ src/index.js
@@ -1,19 +1,46 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
-const typeDefs = `
-type Query {
-    info: String!
+async function main() {
+
+    //Create a new linnk
+    const newLink = await prisma.createLink({
+        url: 'www.prisma.io',
+        description: 'Prisma replaces traditional ORMs',
+    })
+    console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`)
+
+    //Read all the links from the database and print them to the console
+    const allLinks = await prisma.links()
+    console.log(allLinks)
 }
-`
+main().catch(e => console.log.error(e))
 
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews Clone`
-    }
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: (root, args, context, info) => {
+            return context.prisma.links()
+        }
+    },
+    Mutation: {
+        post: (root, args, context) => {
+            return context.prisma.createLink({
+                url: args.url,
+                description: args.description
+            })
+        }
+    },
+    /*Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url
+    }*/
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
+    context: { prisma }
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
